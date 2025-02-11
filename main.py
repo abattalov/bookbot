9471188815ed5ff253593c26f1c07e5235d38ccf
abattalov <abattalov14@gmail.com>
2025-02-10 19:46:43 -0600

    added letter count summary report

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def main():
         unique_letter_count = {}
 
         for letter in text:
-            if letter in unique_letter_count:
-                unique_letter_count[letter] += 1
-            else:
-                unique_letter_count[letter] = 1
+            if letter.isalpha():
+                if letter in unique_letter_count:
+                    unique_letter_count[letter] += 1
+                else:
+                    unique_letter_count[letter] = 1
       
         
         return unique_letter_count
@@ -19,9 +20,20 @@ def main():
     
     count = get_unique_letters(words)
 
-    print(count)
+    list_of_dicts = []
 
-   
+    for key in count:
+        dict = {key: count[key]}
+        list_of_dicts.append(dict)
+    
+    sorted_list = sorted(list_of_dicts, key=lambda x: list(x.values())[0], reverse=True)
+
+    # print(sorted_list)
+
+    for item in sorted_list:
+        key = list(item.keys())[0]
+        value = list(item.values())[0]
+        print(f"The '{key}' character was found {value} times")
 
 main()
 
